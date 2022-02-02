de90d317094b39972afa77cd034c5a32bc3e5cbf
Mazharul-haq <70336576+Mazharul-haq@users.noreply.github.com>
2022-02-03 00:03:04 +0600

    Initial

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/5', function () {
     return view('welcome');
 })->name('home');
 Route::get('/details', function () {
